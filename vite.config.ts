5e4ef3e2c0249439bc3ad17eb20d428517a8ecac
Kaungsetzan <100002922@stud.srh-university.de>
2025-07-16 23:23:36 +0200

    Deploy latest build

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-spa-mittweida/', // Keep this if your repo name is 'my-spa-mittweida'
+  base: '/my-spa-mittweida/',
   build: {
     outDir: 'docs'
   },
